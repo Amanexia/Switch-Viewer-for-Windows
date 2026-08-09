62159cad09bf7941affcecfa3d9b6a5603d6701e
Amanexia <alxiarr@gmail.com>
2026-08-09 12:38:51 +0900

    .\MainWindow.xaml.csの修正

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
+using Window = System.Windows.Window;
 
 namespace SwitchViewer;
 
